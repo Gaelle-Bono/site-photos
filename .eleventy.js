67b5836cf69c947806cc01c9aeb8b83157aaeffb
Gaelle Bono <gaelle.bono@free.fr>
2024-05-25 15:34:09 +0200

    changing sass configuration

--- .eleventy.js
+++ .eleventy.js
@@ -6,10 +6,12 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/uploads');
   eleventyConfig.addPassthroughCopy('src/js');
   eleventyConfig.addWatchTarget('src/js');
+  eleventyConfig.addPassthroughCopy('src/sass');
+  eleventyConfig.addWatchTarget('src/sass');
 
   // Watch CSS files for changes
   eleventyConfig.setBrowserSyncConfig({
-    files: './_site/css/*.css'
+    files: '_site/sass/*.css'
   });
 
   eleventyConfig.addFilter("removeExt", function(name) {
