c351e5d6b36f7c807015800e41959d70ca3a2cd4
Gaelle Bono <gaelle.bono@free.fr>
2024-05-25 18:35:53 +0200

    uninstall sass

--- .eleventy.js
+++ .eleventy.js
@@ -6,13 +6,8 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/uploads');
   eleventyConfig.addPassthroughCopy('src/js');
   eleventyConfig.addWatchTarget('src/js');
-  eleventyConfig.addPassthroughCopy('src/sass');
-  eleventyConfig.addWatchTarget('src/sass');
-
-  // Watch CSS files for changes
-  eleventyConfig.setBrowserSyncConfig({
-    files: '_site/sass/*.css'
-  });
+  eleventyConfig.addPassthroughCopy('src/css');
+  eleventyConfig.addWatchTarget('src/css');
 
   eleventyConfig.addFilter("removeExt", function(name) {
     return name.slice(name.lastIndexOf("/")+1, name.length-4);
